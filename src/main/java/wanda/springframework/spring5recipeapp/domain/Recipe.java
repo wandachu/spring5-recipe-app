5759fdf06b07a0b0244942e9fc5f5a6ac22b1cf5
Wanda Wang <chuwang@wustl.edu>
2022-05-07 10:57:04 -0700

    Refactor pojo to simpilify setters

--- src/main/java/wanda/springframework/spring5recipeapp/domain/Recipe.java
+++ src/main/java/wanda/springframework/spring5recipeapp/domain/Recipe.java
@@ -148,6 +148,13 @@ public class Recipe {
 
   public void setNotes(Notes notes) {
     this.notes = notes;
+    notes.setRecipe(this);
+  }
+
+  public Recipe addIngredient(Ingredient ingredient) {
+    this.ingredients.add(ingredient);
+    ingredient.setRecipe(this);
+    return this;
   }
 
   public Set<Category> getCategories() {
