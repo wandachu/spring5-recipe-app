3f09de96f0dba0a27d3ab91e93c9bf73d6b7c5f8
Wanda Wang <chuwang@wustl.edu>
2022-05-15 20:47:34 -0700

    Add deleteIngredient function and back to Ingredient list function

--- src/main/java/wanda/springframework/spring5recipeapp/controllers/IngredientController.java
+++ src/main/java/wanda/springframework/spring5recipeapp/controllers/IngredientController.java
@@ -76,4 +76,12 @@ public class IngredientController {
     log.debug("saved ingredient id:" + savedCommand.getId());
     return "redirect:/recipe/" + savedCommand.getRecipeId() + "/ingredient/" + savedCommand.getId() + "/show";
   }
+
+  @GetMapping
+  @RequestMapping("/recipe/{recipeId}/ingredient/{id}/delete")
+  public String deleteIngredient(@PathVariable String recipeId, @PathVariable String id) {
+    log.debug("deleting ingredient id: " + id);
+    ingredientService.deleteByIngredientId(Long.valueOf(recipeId), Long.valueOf(id));
+    return "redirect:/recipe/" + recipeId + "/ingredients";
+  }
 }
