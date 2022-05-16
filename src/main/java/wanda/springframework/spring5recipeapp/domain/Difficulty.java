5e25d1eba0c91a594e1f1c3cd048b1e33a203ec8
Wanda Wang <chuwang@wustl.edu>
2022-05-15 21:50:30 -0700

    Fix bootstrap issue

--- src/main/java/wanda/springframework/spring5recipeapp/domain/Difficulty.java
+++ src/main/java/wanda/springframework/spring5recipeapp/domain/Difficulty.java
@@ -1,5 +1,15 @@
 package wanda.springframework.spring5recipeapp.domain;
 
 public enum Difficulty {
-  EASY, MODERATE, HARD
+  EASY("Easy"), MODERATE("Moderate"), HARD("Hard");
+
+  private final String displayValue;
+
+  Difficulty(String displayName) {
+    this.displayValue = displayName;
+  }
+
+  public String getDisplayValue() {
+    return displayValue;
+  }
 }
