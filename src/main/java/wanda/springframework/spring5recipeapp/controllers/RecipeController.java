e84e941c8564323a697bf7ee129c56528943f7f8
Wanda Wang <chuwang@wustl.edu>
2022-05-16 10:14:27 -0700

    Update form html font

--- src/main/java/wanda/springframework/spring5recipeapp/controllers/RecipeController.java
+++ src/main/java/wanda/springframework/spring5recipeapp/controllers/RecipeController.java
@@ -8,11 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import wanda.springframework.spring5recipeapp.commands.RecipeCommand;
 import wanda.springframework.spring5recipeapp.services.RecipeService;
 
 @Controller
 @Slf4j
+@RequestMapping("/recipe")
 public class RecipeController {
   private final RecipeService recipeService;
 
@@ -21,28 +23,28 @@ public class RecipeController {
     this.recipeService = recipeService;
   }
 
-  @GetMapping("/recipe/{id}/show")
+  @GetMapping("/{id}/show")
   public String showById(Model model, @PathVariable String id) {
     model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
     return "recipe/show";
   }
 
-  @GetMapping("/recipe/new")
+  @GetMapping("/new")
   public String newRecipe(Model model) {
     model.addAttribute("recipe", new RecipeCommand());
     return "recipe/recipeform";
   }
 
-  @GetMapping("/recipe/{id}/update")
-  public String updateRecipe(Model model, @PathVariable String id) {
-    model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
-    return "recipe/recipeform";
-  }
-
-  @PostMapping("/recipe")
+  @PostMapping("/save")
   public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
     RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
     return "redirect:/recipe/" + savedCommand.getId() + "/show";
+  }
+
+  @GetMapping("/{id}/update")
+  public String updateRecipe(Model model, @PathVariable String id) {
+    model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
+    return "recipe/recipeform";
   }
 
   @GetMapping("/recipe/{id}/delete")
